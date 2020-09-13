1e804f31b8418c3aa6e35101a6f65453cae75f1a
imomark <khanomarkhalid@gmail.com>
2020-09-13 19:22:28 +0530

    Basic structure

---
+++ src/test/java/com/omar/springrecipebook/SpringRecipeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.springrecipebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipeBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
