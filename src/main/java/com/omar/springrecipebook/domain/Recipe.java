dbde00e3136b31f211608a9488004b44a44fad61
imomark <khanomarkhalid@gmail.com>
2020-09-16 09:33:35 +0530

    JPA added

--- src/main/java/com/omar/springrecipebook/domain/Recipe.java
+++ src/main/java/com/omar/springrecipebook/domain/Recipe.java
@@ -28,6 +28,14 @@ public class Recipe {
     @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
 
+    @ManyToMany
+    @JoinTable(
+            name = "recipe_categories",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private Set<Category> categories;
+
     public Long getId() {
         return id;
     }
